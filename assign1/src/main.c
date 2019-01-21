e424a6837aa8daba05508faf7f13cd7fddaf7bed
Joseph Coffa <josephco440@gmail.com>
2019-01-21 10:34:24 -0500

    Removed old test cases, added new ones to test readFold() and changed functionality

--- assign1/src/main.c
+++ assign1/src/main.c
@@ -192,27 +192,6 @@ int main() {
 
 
 
-    printf("\n----------PRINT FUNCTIONS----------\n");
-    char *toBePrinted = printEvent("");
-    printf("printEvent = %s\n", toBePrinted);
-    free(toBePrinted);
-
-    toBePrinted = printAlarm("");
-    printf("printAlarm = %s\n", toBePrinted);
-    free(toBePrinted);
-
-    toBePrinted = printProperty("");
-    printf("printProperty = %s\n", toBePrinted);
-    free(toBePrinted);
-
-    toBePrinted = printDate("");
-    printf("printDate = %s\n", toBePrinted);
-    free(toBePrinted);
-
-
-
-
-
     printf("\n----------TOUPPER FUNCTIONS----------\n");
     char randCase[] = "ThiS CaSE is RandoM. HOPEFUllY thE FuncTioN WorKS ThIS TImE!";
     printf("Before: \"%s\"\n", randCase);
@@ -227,9 +206,102 @@ int main() {
 
 
 
-    printf("\n\n\n----------LIST TESTING----------\n");
-    printf("----------EVENT LIST----------\n");
 
+    printf("\n\n\n----------FILE PARSE UNFOLDING----------\n");
+    FILE *testUnfold = fopen("/home/joseph/cis2750/a01/testFiles/unfolding.ics", "r");
+    char unfolded[2000];
+    while (!feof(testUnfold)) {
+        readFold(unfolded, 2000, testUnfold);
+        printf("Unfolded line: \"%s\"\n", unfolded);
+    }
+
+    printf("\n");
+
+    FILE *multLineProp = fopen("/home/joseph/cis2750/a01/testFiles/mLineProp1.ics", "r");
+    while (!feof(multLineProp)) {
+        readFold(unfolded, 2000 ,multLineProp);
+        printf("Unfolded line: \"%s\"\n", unfolded);
+    }
+
+    printf("\n");
+
+    FILE *testUnfold2 = fopen("/home/joseph/cis2750/a01/testFiles/newCalendar.ics", "r");
+    while (!feof(testUnfold2)) {
+        readFold(unfolded, 2000, testUnfold2);
+        printf("Unfolded line: \"%s\"\n", unfolded);
+    }
+
+    fclose(testUnfold);
+    fclose(multLineProp);
+    fclose(testUnfold2);
+
+    /*
+    printf("\n\n\n----------LIST TESTING----------\n");
+    printf("----------EVENT AND PROPERTY LIST IN A CALENDAR----------\n");
+    // Create a new calendar by hand, without parsing a file (it's just for testing anyways!)
+    Calendar *testCal = malloc(sizeof(Calendar));
+    testCal->events = initializeList(printEvent, deleteEvent, compareEvents);
+    testCal->properties = initializeList(printProperty, deleteProperty, compareProperties);
+
+    // Create some properties
+    char pd1[] = "CN=Joseph Coffa:mailto:jcoffa@uoguelph.ca";
+    Property *prop1 = malloc(sizeof(Property) + strlen(pd1) + 1);
+    strcpy(prop1->propName, "ORGANIZER");
+    strcpy(prop1->propDescr, pd1);
+
+    char pd2[] = "20190120T194500Z";
+    Property *prop2 = malloc(sizeof(Property) + strlen(pd2) + 1);
+    strcpy(prop2->propName, "DTSTART");
+    strcpy(prop2->propDescr, pd2);
+
+    // add them to the Calendar's property lists
+    insertFront(testCal->properties, (void *)prop1);
+    insertFront(testCal->properties, (void *)prop2);
+
+    // create some events
+    DateTime create1;
+    strcpy(create1.date, "20180119");
+    strcpy(create1.time, "033400");
+    create1.UTC = true;
+
+    DateTime start1;
+    strcpy(start1.date, "20180122");
+    strcpy(start1.time, "024500");
+    start1.UTC = true;
+
+    Event *ev1 = malloc(sizeof(Event));
+    strcpy(ev1->UID, "040000008200E00074C5B7101A82E00800000000279CEA4C96511D4BB94C80E44E9DF21210000000C10B52D0760D2E41BB9821D392EF8158");
+    ev1->creationDateTime = create1;
+    ev1->startDateTime = start1;
+    ev1->properties = initializeList(printProperty, deleteProperty, compareProperties);
+    ev1->alarms = initializeList(printAlarm, deleteAlarm, compareAlarms);
+
+    char *evPDscr1 = "Sample property description";
+    Property *evP1 = malloc(sizeof(Property) + strlen(evPDscr1) + 1);
+    strcpy(evP1->propName, "This is a sample name");
+    strcpy(evP1->propDescr, evPDscr1);
+
+    insertFront(ev1->properties, evP1);
+
+    char *evADscr1 = "Sample alarm trigger";
+    Alarm *evA1 = malloc(sizeof(Alarm));
+    evA1->properties = initializeList(printProperty, deleteProperty, compareProperties);
+    strcpy(evA1->action, "This is a sample action");
+    evA1->trigger = malloc(strlen(evADscr1) + 1);
+    strcpy(evA1->trigger, evADscr1);
+
+    insertFront(ev1->alarms, evA1);
+
+    
+    insertFront(testCal->events, ev1);
+
+    // print and free Calendar string
+    char *calToStr = printCalendar(testCal);
+    printf("Calendar: \"%s\"\n", calToStr);
+    free(calToStr);
+
+    deleteCalendar(testCal);
+    */
 
     return EXIT_SUCCESS;
 }
